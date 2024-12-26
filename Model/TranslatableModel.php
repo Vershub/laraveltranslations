62e6a3fc4c62f43ac64b9e5e2b85fe5b9bbe3140
Andrey Kovtonyuk <andreykovtonyuk@Andreys-MacBook-Air.local>
2024-12-26 14:36:27 +0200

    Initial commit

---
+++ Model/TranslatableModel.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Vershub\laraveltranslations\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+abstract class TranslatableModel extends Model
+{
+    abstract protected function getTranslationModel(): string;
+
+    abstract protected function getForeignKeyForTranslation(): string;
+
+    protected function getLocaleCodeColumn(): string
+    {
+        return 'locale_code';
+    }
+
+    public function translation(): BelongsTo
+    {
+        return $this->belongsTo($this->getTranslationModel());
+    }
+
+    public function scopeWithTranslation($query, string $localeCode = null): void
+    {
+        $localeCode = $localeCode ?: app()->getLocale();
+
+        $query->addSelect([
+            'current_translate_id' => $this->getTranslationModel()::select('id')
+                ->whereColumn($this->getForeignKeyForTranslation(), "{$this->getTable()}.id")
+                ->where($this->getLocaleCodeColumn(), $localeCode)
+                ->take(1)
+        ])->with('translation');
+    }
+
+}
---
+++ src/LaravelTranslationsServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Vershub\LaravelTranslations;
+
+use Illuminate\Support\ServiceProvider;
+class LaravelTranslationsServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+}
